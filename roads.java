4d435889542172fe6933557e7125f85b43cb5640
Ben Chase <ben.chase@benchase.tk>
2021-03-05 13:51:19 -0500

    2021.03.05

--- roads.java
+++ roads.java
@@ -2,19 +2,23 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class roads {
-  public static void main(String[] args) {
+public class roads
+{
+  public static void main(String[] args)
+  {
 
     Scanner input = new Scanner(System.in);
     int choice = 1;
 
-  while (choice == 1) {
+  while (choice == 1)
+  {
     System.out.print("Enter region: "); // region code as on website
     String region = input.nextLine();
     System.out.print("Enter route:  "); // same as summer 2020 code
     String road = input.nextLine();
 
-    try {
+    try
+    {
       File obj = new File(region + "/" + road + ".txt");
       Scanner read = new Scanner(obj);
       while (read.hasNextLine()) {
@@ -22,7 +26,9 @@ public class roads {
         System.out.println(data);
       }
       read.close();
-    } catch (FileNotFoundException e) {
+    }
+    catch (FileNotFoundException e) 
+    {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
